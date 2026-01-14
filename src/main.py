ea29ee8d6b790520c750a766e89b14e7daa40b0a
Maksym Viacheslavovych <maximbutow25@gmail.com>
2026-01-14 15:30:58 +0200

    Added config files

---
+++ src/main.py
@@ -0,0 +1,44 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import Command
+from aiogram.types import LinkPreviewOptions
+
+from config import settings
+from src.utils.schedule_utils import ScheduleProcessor
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=settings.bot_token,
+          default=DefaultBotProperties(parse_mode=ParseMode.HTML, link_preview=LinkPreviewOptions(is_disabled=True)))
+dp = Dispatcher()
+
+schedule_processor = ScheduleProcessor()
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    text = schedule_processor.generate_schedule_message_for_week(True)
+    await message.answer(text)
+
+
+@dp.message(Command("current_week"))
+async def cmd_current_week(message: types.Message):
+    text = schedule_processor.generate_schedule_message_for_week(True)
+    await message.answer(text)
+
+
+@dp.message(Command("next_week"))
+async def cmd_next_week(message: types.Message):
+    text = schedule_processor.generate_schedule_message_for_week(False)
+    await message.answer(text)
+
+async def main():
+    print()
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
